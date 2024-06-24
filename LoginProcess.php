047a3b9db1ba9c010f69d0b53fb59233b952e883
nr01adi <162496054+nr01adi@users.noreply.github.com>
2024-06-24 19:40:30 +0800

    LoginProcess.php

--- LoginProcess.php
+++ LoginProcess.php
@@ -1,7 +1,7 @@
 <?php
-
 session_start();
 
+// Database credentials
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -19,6 +19,9 @@ if ($conn->connect_error) {
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+// Prevent SQL Injection
+$username = $conn->real_escape_string($username);
+
 // Check user credentials
 $sql = "SELECT * FROM users WHERE username='$username'";
 $result = $conn->query($sql);
@@ -26,14 +29,10 @@ $result = $conn->query($sql);
 if ($result->num_rows == 1) {
     $row = $result->fetch_assoc();
     if (password_verify($password, $row['password'])) {
-        // Check if user is already logged in
-        if (!isset($_SESSION['username'])) {
-            $_SESSION['username'] = $username; // Store username in session for one-time login
-            session_unset(); // remove all stored values in session variables
-            session_destroy(); // Destroys all data registered to a session
-            session_write_close(); // End the current session and store session data.
-            setcookie(session_name(),'',0,'/'); // Send Cookie to client web browser.
-        }
+        // Set session variable
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+        
         // Redirect to Index.php
         header("Location: Index.php");
         exit;
